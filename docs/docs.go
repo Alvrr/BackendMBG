e58beb6688f61e5b8d66378a47ff0663ae441df6
farid <dekfarid2015@gmail.com>
2025-07-21 02:02:48 +0700

    coba ke 9999

--- docs/docs.go
+++ docs/docs.go
@@ -1459,7 +1459,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:5000",
+	Host:             "backendmbg-production.up.railway.app",
 	BasePath:         "/",
 	Schemes:          []string{"http", "https"},
 	Title:            "Backend MBG API",
