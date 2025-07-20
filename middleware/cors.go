f8bd3c52458f7826e02f51e1339dc2cd98e10647
farid <dekfarid2015@gmail.com>
2025-07-21 01:46:53 +0700

    bissmilahhh

--- middleware/cors.go
+++ middleware/cors.go
@@ -7,8 +7,9 @@ import (
 
 func CorsMiddleware() fiber.Handler {
 	return cors.New(cors.Config{
-		AllowOrigins: "https://frontend-mbg.vercel.app", // Ganti sesuai URL frontend kamu
-		AllowHeaders: "Origin, Content-Type, Accept, Authorization",
-		AllowMethods: "GET,POST,PUT,DELETE",
+		AllowOrigins:     "https://frontend-mbg.vercel.app,https://backendmbg-production.up.railway.app,http://localhost:5000,https://localhost:5000", // Frontend + Railway domain + localhost untuk Swagger testing
+		AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
+		AllowMethods:     "GET,POST,PUT,DELETE",
+		AllowCredentials: true,
 	})
 }
